88626191018ccd3f8ec9db138f8e5e84634d1754
INEEDMONEY$ <121662005+INEEDDMONEYY@users.noreply.github.com>
2025-09-16 01:37:39 +0000

    Fixing a style problem causing OCD.

--- onex/src/pages/signInPage.jsx
+++ onex/src/pages/signInPage.jsx
@@ -7,7 +7,7 @@ export default function SignIn() {
         <div className="flex flex-col signin-bg h-screen p-[200px] overflow-hidden place-content-center">
             <div className="inline-block bg-gray-300 text-center rounded-lg w-96 self-center p-2 form-bg">
                 <div className="flex place-items-center justify-self-center align-center text-center">
-                    <h1 className="text-black text-[2rem]">Mystery Mansion</h1>
+                    <h1 className="text-black text-[2rem] text-center">Mystery Mansion</h1>
                     <img src={SigninLogo} alt="" className="signin-logo" />
                 </div>
                 <h3 className="text-black text-[2rem]">Sign In</h3>
